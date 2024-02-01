559e6fd189f10dc0fd8db82fe5da76cbf2bad0c9
Usman Butt <usman.butt@outlook.com>
2024-02-01 16:29:01 -0500

    Refactor DrumMachine component to use soundEnabled hook

--- src/components/DrumMachine/DrumMachine.js
+++ src/components/DrumMachine/DrumMachine.js
@@ -6,7 +6,10 @@ import styles from './DrumMachine.module.css';
 
 const SOUND_SRC = '/909-drums.mp3';
 
+import { useSoundEnabled } from '../../hooks/useSoundEnabled';
+
 function DrumMachine() {
+  const { soundEnabled } = useSoundEnabled();
   const [play] = useSound(SOUND_SRC, {
     sprite: {
       kick: [0, 350],
@@ -14,31 +17,15 @@ function DrumMachine() {
       snare: [666, 290],
       cowbell: [968, 200],
     },
-    soundEnabled: true,
+    soundEnabled: soundEnabled,
   });
 
   return (
     <div className={styles.wrapper}>
-      <button
-        onClick={() => play({ id: 'kick' })}
-      >
-        Kick
-      </button>
-      <button
-        onClick={() => play({ id: 'hihat' })}
-      >
-        Hat
-      </button>
-      <button
-        onClick={() => play({ id: 'snare' })}
-      >
-        Snare
-      </button>
-      <button
-        onClick={() => play({ id: 'cowbell' })}
-      >
-        Cowbell
-      </button>
+      <button onClick={() => play({ id: 'kick' })}>Kick</button>
+      <button onClick={() => play({ id: 'hihat' })}>Hat</button>
+      <button onClick={() => play({ id: 'snare' })}>Snare</button>
+      <button onClick={() => play({ id: 'cowbell' })}>Cowbell</button>
     </div>
   );
 }
